981555d06b876aef479c197c83824b6bd80b7717
RTimmerGH <timmer.romain@gmail.com>
2023-12-21 16:11:54 +0100

    main spring project initialization

---
+++ src/main/java/com/wcs/mtgbox/MtgboxApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.mtgbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgboxApplication.class, args);
+	}
+
+}
