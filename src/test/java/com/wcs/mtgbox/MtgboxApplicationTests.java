c75d71cc6fc79af43c0e6c5c6bff7e390aed1000
Romain Timmer <romain-timmer_student2023@wilder.school>
2023-12-21 16:29:26 +0100

    main spring project initialization (#4)

---
+++ src/test/java/com/wcs/mtgbox/MtgboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.mtgbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
