6a269a7ad715c82e86eda33276968060e56a512b
Antoine <123865300+cassaga@users.noreply.github.com>
2024-02-22 14:17:47 +0100

    Add  get collection card (#50)
    * Add  get collection card
    
    * Add  get collection card

--- src/main/java/com/wcs/mtgbox/collection/infrastructure/repository/UserCardRepository.java
+++ src/main/java/com/wcs/mtgbox/collection/infrastructure/repository/UserCardRepository.java
@@ -4,5 +4,8 @@ import com.wcs.mtgbox.collection.domain.entity.Card;
 import com.wcs.mtgbox.collection.domain.entity.UserCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserCardRepository extends JpaRepository<UserCard, Long> {
+    List<UserCard> findAllByUserId(Long userId);
 }
