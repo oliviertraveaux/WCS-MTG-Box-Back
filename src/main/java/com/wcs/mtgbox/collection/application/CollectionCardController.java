291ac4dad816f90357acf4a3924f9bb9de033c2e
Antoine <cassagnea@gmail.com>
2024-02-22 11:32:16 +0100

    Add  get collection card

--- src/main/java/com/wcs/mtgbox/collection/application/CollectionCardController.java
+++ src/main/java/com/wcs/mtgbox/collection/application/CollectionCardController.java
@@ -1,6 +1,7 @@
 package com.wcs.mtgbox.collection.application;
 
 import com.wcs.mtgbox.collection.domain.dto.AddCollectionCardDto;
+import com.wcs.mtgbox.collection.domain.dto.CollectionCardDto;
 import com.wcs.mtgbox.collection.domain.service.CollectionCardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +21,9 @@ public class CollectionCardController {
     }
 
     @GetMapping("/{id}")
-    boolean readAllCollectionCardsFromUser(@PathVariable Long id) throws Exception{
-        return true;
-    }
+    public ResponseEntity<List<CollectionCardDto>> readAllCollectionCardsFromUser(@PathVariable Long id) {
+        List<CollectionCardDto> cards = collectionCardService.getCollectionCardsByUserId(id);
+        return ResponseEntity.ok(cards);}
 
     @PostMapping
     public ResponseEntity<?> addCollectionCards(@RequestBody List<AddCollectionCardDto> addCardsBody)  {
