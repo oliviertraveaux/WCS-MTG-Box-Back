f665a66bd320041959fa0ae1c8ac55a63344990b
Olivier Traveaux <olivier.traveaux@gmail.com>
2024-05-15 17:32:50 +0200

    feat: update lastConnectionDate when verify token (#93)

--- src/main/java/com/wcs/mtgbox/auth/domain/entity/User.java
+++ src/main/java/com/wcs/mtgbox/auth/domain/entity/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 import org.springframework.data.annotation.CreatedDate;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@DynamicUpdate
 @Table(name = "`user`")
 @EntityListeners(AuditingEntityListener.class)
 @Getter @Setter
