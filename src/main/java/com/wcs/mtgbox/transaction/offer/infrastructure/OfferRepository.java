f6c861c691593d94313a285f89929517a7f1ece9
RTimmerGH <timmer.romain@gmail.com>
2024-07-17 10:26:55 +0200

    fix - offers with same userCard than a validated offer are now properly deleted

--- src/main/java/com/wcs/mtgbox/transaction/offer/infrastructure/OfferRepository.java
+++ src/main/java/com/wcs/mtgbox/transaction/offer/infrastructure/OfferRepository.java
@@ -2,6 +2,8 @@ package com.wcs.mtgbox.transaction.offer.infrastructure;
 
 import com.wcs.mtgbox.transaction.offer.domain.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface OfferRepository extends JpaRepository<Offer, Long> {
     List<Offer> findAllByUser_IdOrderByLastModificationDateDesc(Long userId);
     List<Offer> findAllByWantedUserCard_User_IdOrderByCreatedDateDesc(Long userId);
     List<Offer> findAllByUser_IdOrWantedUserCard_User_IdOrderByLastModificationDateDesc(Long userId, Long wantedUserCardUserId);
+    @Query(value = "SELECT o FROM Offer o JOIN o.userCards uc WHERE uc.id = :userCardId")
+    List<Offer> findOffersByUserCardId(@Param("userCardId") Long userCardId);
 }
